a7d00afcca3658882799c94122210a10b919d276
Christopher MacGown <ignoti+github@gmail.com>
2018-01-15 13:05:29 -0800

    Add Lock example

--- rust/examples/lock/src/main.rs
+++ rust/examples/lock/src/main.rs
@@ -1,33 +1,15 @@
 #[macro_use]
-extern crate metaparticle_sync as sync;
+extern crate metaparticle_sync;
 
-#[macro_use]
-extern crate emit;
 
-use std::thread;
-use std::time;
-
-use emit::PipelineBuilder;
-use emit::collectors::stdio::StdioCollector;
-use emit::formatters::raw::RawFormatter;
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
-    let _flush = PipelineBuilder::new()
-        .write_to(StdioCollector::new(RawFormatter::new()))
-        .init();
-
-    let lock = lock!("hello-world");
-    lock.lock(|| {
-        let duration = time::Duration::new(1,0);
-        info!("Sleeping! {}", duration: duration.as_secs());
-        thread::sleep(time::Duration::new(1,0));
-        info!("Waking up! {}", lock: "some-lock")
-    });
-
-    lock!("hello-world", { 
-        let duration = time::Duration::new(1,0);
-        info!("Sleeping! {}", duration: duration.as_secs());
-        thread::sleep(time::Duration::new(1,0));
-        info!("Waking up! {}", lock: "some-lock")
-    });
+    // This will retry ten times before failing.
+    lock!("example-lock", 
+          || {
+              println!("Just an example lock");
+              sleep(Duration::from_millis(250))
+          });
 }
